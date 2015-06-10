a4ad89cda2dfabd93bc168ec1f844c051cd7c534
antonaberg <antonaberg@hotmail.com>
2015-06-10 19:55:47 +0200

    Create add.php

---
+++ add.php
@@ -0,0 +1,15 @@
+<?php
+echo' 
+<html>
+<head>
+</head>
+<body>
+<form action="add2.php" method="post">
+name <input type="text" name="name">
+phone <input type="text" name="phone">
+<input type="submit" value="submit">
+</body>
+</html>
+';
+ 
+?>
