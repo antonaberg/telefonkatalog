7c2399126e25b4a6d138d07b73b752d9e8308b63
antonaberg <antonaberg@hotmail.com>
2015-06-10 19:57:16 +0200

    Create search.php

---
+++ search.php
@@ -0,0 +1,19 @@
+<?php
+echo' 
+<html>
+<head>
+</head>
+<body>
+<h2> search for name or phone <h2>
+<form action="search2.php" method="post">
+name <input type="text" name="name2">
+<input type="submit" value="submit">
+<br>
+<form action="search2.php" method="post">
+phone <input type="text" name="phone2">
+<input type="submit" value="submit">
+</body>
+</html>
+';
+ 
+?>
